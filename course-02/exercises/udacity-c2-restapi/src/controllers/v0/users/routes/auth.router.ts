a7aa86eed5d23b4355da78efd165b748bb2c7009
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-12-29 12:18:18 -0800

    Added necessary Changes

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -29,7 +29,11 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 
 function generateJWT(user: User): string {
     //@TODO Use jwt to create a new JWT Payload containing
+<<<<<<< HEAD
     return jwt.sign(user.toJSON(), config.jwt.secret);
+=======
+    return jwt.sign(user, config.jwt.secret);
+>>>>>>> Added necessary Changes
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
